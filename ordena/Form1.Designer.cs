aa8e1e4658d2384069bb3d1df72771a0a9b9cb75
Jmendezn <jhmn_@hotmail.com>
2018-12-22 13:28:58 -0700

    22-12

---
+++ ordena/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ordena
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 290);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(140, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 251);
+            this.listBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "QS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 311);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(269, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(269, 111);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(70, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(51, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "SHELL";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(137, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 39);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(266, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 34);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "TOTAL DE ITRACIONES METODO QUICKSHORT";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(266, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "TOTAL DE ITERACIONES METODO SHELL\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 336);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ordena/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ordena
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+        
+        InitializeComponent();
+        }
+        private int[] vector;
+        int iX = 50000;
+        int i = 0;
+        int ii = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int auxili;
+            int j;
+            ii = 0;
+
+            Random r = new Random();
+
+            vector = new int[iX];
+            for (i = 0; i < iX; i++)
+            {
+              
+                vector[i] = r.Next(10, 101);
+
+            }
+
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            label1.Text = "ITERACIONES METODO QUICKSHORT";
+            for (int i = 0; i < iX; i++)
+            {
+                listBox1.Items.Add(vector[i]);
+                // listBox2.Items.Add(r.Next(10, 101));
+
+            }
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                auxili = vector[i];
+                j = i - 1;
+                while (j >= 0 && vector[j] > auxili)
+                {
+                    vector[j + 1] = vector[j];
+                    ii = ii + 1;
+                    
+                    textBox1.Text = Convert.ToString(ii);
+                    j--;
+                }
+                vector[j + 1] = auxili;
+            }
+
+            for (int i = 0; i < iX; i++)
+            {
+                listBox2.Items.Add(vector[i]);
+             
+            }
+            textBox2.Text = textBox1.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+            for (int i = 0; i < 10; i++)
+            {
+                listBox1.Items.Add(vector[i]);
+                // listBox2.Items.Add(r.Next(10, 101));
+                
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int salto = 0;
+            int sw = 0;
+            int auxi = 0;
+            int E1 = 0;
+            ii = 0;
+
+            Random r = new Random();
+
+            vector = new int[iX];
+            for (i = 0; i < iX; i++)
+            {
+               
+                vector[i] = r.Next(10, 101);
+
+            }
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            textBox1.Text = "";
+            textBox3.Text = "";
+            salto = vector.Length / 2;
+            label1.Text = "ITERACIONES METODO SHELL";
+            for (int i = 0; i < iX; i++)
+            {
+                listBox1.Items.Add(vector[i]);
+               
+
+            }
+
+            while (salto > 0)
+            {
+                sw = 1;
+                while (sw != 0)
+                {
+                    sw = 0;
+                    E1 = 1;
+                    while (E1 <= (vector.Length - salto))
+                    {
+                        if (vector[E1 - 1] > vector[(E1 - 1) + salto])
+                        {
+                            auxi = vector[(E1 - 1) + salto];
+                            vector[(E1 - 1) + salto] = vector[E1 - 1];
+                            vector[(E1 - 1)] = auxi;
+                            sw = 1;
+                            ii = ii + 1;
+                            textBox1.Text = Convert.ToString(ii);
+                        }
+                        E1++;
+                    }
+                }
+                salto = salto / 2;
+
+            }
+            for (int i = 0; i < iX; i++)
+            {
+                listBox2.Items.Add(vector[i]);
+                // listBox2.Items.Add(r.Next(10, 101));
+
+            }
+            textBox3.Text = textBox1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
